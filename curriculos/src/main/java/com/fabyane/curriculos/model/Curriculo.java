474f9dca44ee2ff2ddadd3a32b8dc845ee145bc3
Fabyane Nayara <113144650+FabyaneNayaraO@users.noreply.github.com>
2023-12-14 11:22:35 -0300

    Update Curriculo.java

--- curriculos/src/main/java/com/fabyane/curriculos/model/Curriculo.java
+++ curriculos/src/main/java/com/fabyane/curriculos/model/Curriculo.java
@@ -9,7 +9,6 @@ import lombok.Data;
 
 @Data
 @Entity
-@Table(name = "curriculos")
 public class Curriculo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,11 +19,6 @@ public class Curriculo {
     private String objetivo;
     private String experiencia;
 
- 
-    public Curriculo() {
-       
-    }
-
     
     public Curriculo(String nome, String telefone, String email, String objetivo, String experiencia) {
         this.nome = nome;
@@ -34,53 +28,6 @@ public class Curriculo {
         this.experiencia = experiencia;
     }
 
-    // Getters e Setters
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getTelefone() {
-        return telefone;
-    }
-
-    public void setTelefone(String nome) {
-        this.nome = telefone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String nome) {
-        this.nome = email;
-    }
-
-    public String getObjetivo() {
-        return objetivo;
-    }
-
-    public void setObjetivo(String objetivo) {
-        this.objetivo = objetivo;
-    }
-
-    public String getExperiencia() {
-        return experiencia;
-    }
-
-    public void setExperiencia(String experiencia) {
-        this.experiencia = experiencia;
-    }
+   
 }
 
